a78dca6bbb3683f55584c7cebc90d27b6809ccd6
Nongdada <ronnachai_si@cmu.ac.th>
2023-01-06 15:33:08 +0700

    "complete"

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,29 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+ int main(){
+    srand(time(0));
+    int x = rand()%9+1;
+string grade;
+    if(x == 1) grade = "A";
+    else if(x == 2) grade = "B+";
+    else if(x == 3) grade = "B";
+    else if(x == 4) grade = "C+";
+    else if(x == 5) grade = "C";
+    else if(x == 6) grade = "D+";
+    else if(x == 6) grade = "D";
+    else if(x == 8) grade = "F";
+    else grade = "W";
+
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+
+    cout << "You will get " << grade << " in this 261102.";
+    return 0;
+ }
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
